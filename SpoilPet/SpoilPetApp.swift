85a2978c639f6a4551cb70669391446eb069f952
Alex <Alex@steelfoxmedia.com>
2024-05-03 15:36:53 -0500

    Initial Commit

---
+++ SpoilPet/SpoilPetApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpoilPetApp.swift
+//  SpoilPet
+//
+//  Created by Alex Ryan on 5/3/24.
+//
+
+import SwiftUI
+
+@main
+struct SpoilPetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
