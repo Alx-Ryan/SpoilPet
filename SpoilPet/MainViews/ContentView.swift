9669cc8ebe2185ba7f916b0154cc49469f019896
Alex <Alex@steelfoxmedia.com>
2024-05-24 11:18:37 -0500

    [Refactoring] PetsView now has its own view

--- SpoilPet/MainViews/ContentView.swift
+++ SpoilPet/MainViews/ContentView.swift
@@ -8,10 +8,8 @@
 import SwiftUI
 import SDWebImageSwiftUI
 
-
 struct ContentView: View {
     
-    @StateObject var vm2 = CombineVM()
     @State private var tabSelection: TabBarItem = .schedule
     
     var resizingMode: ContentMode = .fill
@@ -20,12 +18,11 @@ struct ContentView: View {
         CustomTabBarContainer(selection: $tabSelection) {
             
                 //MARK: First tab item
-            Color.blue
+            ScheduleView()
                 .tabBarItem(tab: .schedule, selection: $tabSelection)
             
                 //MARK: Second tab item
-            listView
-                .overlay(addPetButton)
+            PetsView()
                 .tabBarItem(tab: .pets, selection: $tabSelection)
             
                 //MARK: Third tab item
@@ -34,106 +31,6 @@ struct ContentView: View {
             
         }
         .background(Color.background)
-    }
-    
-    
-    private var listView: some View {
-        NavigationStack {
-            ZStack {
-                Color.background.ignoresSafeArea()
-                List {
-                    ForEach(Array(Set(vm2.catImages)).prefix(4), id: \.id) { uniqueImage in
-                        Section {
-                            HStack(alignment: .top, spacing: 12) {
-                                if let imageUrl = uniqueImage.sample_url {
-                                    WebImage(url: URL(string: imageUrl))
-                                        .resizable()
-                                        .indicator(.activity)
-                                        .scaledToFill()
-                                        .frame(maxWidth: 100)
-                                        .frame(minWidth: 0)
-                                        .frame(height: 120)
-                                        .clipShape(RoundedRectangle(cornerRadius: 10))
-                                        .allowsHitTesting(false)                            .overlay(
-                                            //Sex add later
-                                            Circle()
-                                                .frame(width: 30, height: 30)
-                                                .offset(x: 30, y: 40)
-                                                .shadow(radius: 10, y: 5)
-                                                .overlay(
-                                                    Image(systemName: "")
-                                                )
-                                        )
-                                    
-                                }
-                                
-                                    // pet details
-                                VStack(alignment: .leading, spacing: 4) {
-                                        // Pet name and Breed
-                                    HStack {
-                                        Text("Name")
-                                            .font(.title3)
-                                            .bold()
-                                            .frame(maxWidth: .infinity, alignment: .leading)
-                                        
-                                        Text("Breed")
-                                            .font(.subheadline)
-                                            .fontWeight(.semibold)
-                                            .foregroundStyle(Color.text.opacity(0.9))
-                                            .padding(.horizontal, 12)
-                                    }
-                                    .padding(.top, 12)
-                                    .padding(.bottom, 12)
-                                    
-                                        // Age
-                                    HStack {
-                                        Image(systemName: "calendar")
-                                        Text("Age")
-                                    }
-                                    .font(.footnote)
-                                    .padding(4)
-                                    .background(RoundedRectangle(cornerRadius: 4).fill(Color.mutedGreen))
-                                    
-                                        // Weight
-                                    HStack {
-                                        Image(systemName: "scalemass")
-                                        Text("Weight")
-                                    }
-                                    .font(.footnote)
-                                    .padding(4)
-                                    .background(RoundedRectangle(cornerRadius: 4).fill(Color.mutedGreen))
-                                    
-                                }
-                            }
-                            
-                        }
-                        
-                    }
-                    .listRowBackground(Color.cell)
-                    .listRowInsets(EdgeInsets(top: 4, leading: 4, bottom: 4, trailing: 4))
-                    
-                    
-                }
-                .shadow(radius: 5, x: 0, y: 4)
-                .scrollContentBackground(.hidden)
-            .foregroundStyle(Color.text)
-        .navigationTitle("My Pets")
-            }
-        }
-
-    }
-    
-    private var addPetButton: some View {
-        VStack {
-            Spacer()
-            HStack() {
-                AddPetButton()
-                    .frame(width: 80, height: 80)
-            }
-        }
-        .frame(maxWidth: .infinity, alignment: .bottomTrailing)
-        .padding(.bottom, 110)
-        .padding(.horizontal, 20)
     }
 }
 
